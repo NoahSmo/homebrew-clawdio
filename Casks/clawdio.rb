a2fadbe2a420fa61a65b32d585fb6cdad51af3a8
NoahSmo <noah.petel.app@gmail.com>
2026-09-24 22:59:29 +0200

    clawdio 0.2.0

--- Casks/clawdio.rb
+++ Casks/clawdio.rb
@@ -1,6 +1,6 @@
 cask "clawdio" do
-  version "0.1.0"
-  sha256 "1e15a7729b762a05b291f7fed5c8b8c1d2149884cb6c24b4cdc96ea5ea6959d8"
+  version "0.2.0"
+  sha256 "ac1cd11ccec9bf9eb9be541477c7815358d65e83b26366fd523c9a08b1b1d8d3"
 
   url "https://github.com/NoahSmo/clawdio/releases/download/v#{version}/Clawdio-#{version}.zip"
   name "Clawdio"
